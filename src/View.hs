7e02216d924bb117da3630e527cbfe843be926ef
Alan Buxton <alanbuxton@gmail.com>
2017-04-22 15:13:03 +0100

    Layout changes; added link to Readme

--- src/View.hs
+++ src/View.hs
@@ -42,6 +42,10 @@ graphPage pm ps cm cs ex pp s1 s2 s3 pls chs maxC maxP = docTypeHtml $ do
     link ! href "/css/styles.css" ! rel "stylesheet" ! type_ "text/css"
   body $ do
     h1 "Effects of Planck vs Chauffeur Knowledge"
+    p $ do
+      "If this is your first time here, then follow this link to "
+      a ! href "https://github.com/alanbuxton/planck-chauffeur/blob/master/README.md" $ "read the background"
+    p "Choose parameters below:"
     inputForm (toValue pm) (toValue ps) (toValue cm) (toValue cs) (toValue ex) (toValue pp) maxP
     div ! id "knowledgeChart" $ mempty
     script $ scoreChart s1 s2 s3
